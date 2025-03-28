f222aba1b6cf78a8393a071ac59cf26dd8fcdac9
Qweez <gettin106@yandex.ru>
2025-03-28 23:20:18 +0300

    final remake 8.0

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const greeting = () => {
   console.log('Welcome to the Brain Games!'); // Поставьте 5 пожалуйста, я старался долго писать код (5)
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
