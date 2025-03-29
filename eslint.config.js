e156d208867e2a9098e96f2476d1e09fff10db31
Qweez <gettin106@yandex.ru>
2025-03-29 06:04:06 +0300

    Final boss remake 3.0

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import js from '@eslint/js';
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ['**/*.{js,mjs,cjs}'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
 ]);
